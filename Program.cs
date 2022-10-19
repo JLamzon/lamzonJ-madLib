9cf14b98472556e29a31cf4cfa6419285c800577
JLamzon <jlamzon@yahoo.com>
2022-10-18 20:24:25 -0700

    notes added

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ while (playAgain != "no")
     Console.WriteLine("Let's play a game of Mad Libs!");
     Console.WriteLine("Rules: type in a word based on the preference needed, and it will turn into a magical story");
 
+    //user input for game
     Console.WriteLine("Please enter a Celebrity name");
     string celeb = Console.ReadLine();
 
@@ -37,7 +38,7 @@ while (playAgain != "no")
     string action = Console.ReadLine();
 
     Console.WriteLine("Okay, here it goes!");
-  
+
     Console.WriteLine("");
     Console.WriteLine($"I woke up this morning and saw {celeb}. I said hey! What are you doing with my {animal}!");
     Console.WriteLine($"{celeb} looked at me and said who are you?! I said I'm {pName} and im a {profession} from {from}.");
@@ -45,10 +46,12 @@ while (playAgain != "no")
     Console.WriteLine($"Realizing i was in {celeb}'s house, I {action} out of bed and grabbed my {plant}.");
     Console.WriteLine("I then woke up.....it was just a silly dream.");
     Console.WriteLine("");
-    
-Console.WriteLine("Thanks for playing!\n");
-Console.WriteLine("Press any key to play again or Type no.....");
-playAgain = Console.ReadLine().ToLower();
+
+    Console.WriteLine("Thanks for playing!\n");
+
+    //this will loop if user chooses to play again or end the game
+    Console.WriteLine("Press any key to play again or Type no.....");
+    playAgain = Console.ReadLine().ToLower();
 }
 
 Console.WriteLine("Thanks for playing!");
